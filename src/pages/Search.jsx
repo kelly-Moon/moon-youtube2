29b479a79adb3f3a43afa824c9e00f45bb9ebaf6
kelly-Moon <kellymoon220@gmail.com>
2025-09-28 02:25:08 +0900

    비디오 상세페이지 만들기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -12,17 +12,18 @@ const Search = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setVideos([]);
-    fetchVideos(searchId);
-    setLoading(true);
+    setVideos([]); // 새 검색어 입력하면 기존 영상 초기화
+    fetchVideos(searchId); // 새로운 검색어로 영상 요청
+    setLoading(true); // 로딩 시작
   }, [searchId]);
+  //searchId가 바뀌면(useParams로 URL에서 가져옴),
 
   const fetchVideos = (query, pageToken = "") => {
     fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
       .then((data) => {
-        setNextPageToken(data.nextPageToken);
-        setVideos((prevVideo) => [...prevVideo, ...data.items]);
-        setLoading(false);
+        setNextPageToken(data.nextPageToken); // 다음 페이지 토큰 저장
+        setVideos((prevVideo) => [...prevVideo, ...data.items]); // 기존 목록 뒤에 새 영상 이어 붙임,
+        setLoading(false); // 로딩 완료
       })
       .catch((error) => {
         console.log("Error fetching data", error);
@@ -32,7 +33,7 @@ const Search = () => {
 
   const handleLoadMore = () => {
     if (nextPageToken) {
-      fetchVideos(searchId, nextPageToken);
+      fetchVideos(searchId, nextPageToken); // 저장해둔 토큰으로 다음 영상 가져오기
     }
   };
 
@@ -40,6 +41,9 @@ const Search = () => {
   return (
     <Main title="유투브 검색" description="유튜브 검색 결과 페이지입니다.">
       <section id="searchPage" className={searchPageClass}>
+        <h2>
+          <em>❤️ {searchId}</em>검색 결과입니다.
+        </h2>
         <div className="video__inner search">
           <VideoSearch videos={videos} />
         </div>
